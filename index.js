3338b994a4eb1352e5ccae5c2a61d79050b71b91
Orrett Bailey <orrettlb@gmail.com>
2017-09-16 06:05:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,3 +22,9 @@ function appendKitten(name){
 function prependKitten(name){
   kittens.unshift(name);
 }
+function removeLastKitten(){
+  kittens.slice(0. kittens.length -1);
+}
+function removeFirstKitten(){
+  kittens.slice(1);
+}
