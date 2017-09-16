851fd98c920391241f843191b7c3f96280dc215a
Orrett Bailey <orrettlb@gmail.com>
2017-09-16 05:52:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
-kittens.push("Butter");
-return kittens;
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
